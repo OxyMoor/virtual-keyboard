a46dbc97ad17c2b78cba26895e1dd8e9363b63cb
SeriakovaOksana <hikki0019@gmail.com>
2023-04-28 13:02:53 +0300

    feat: add function to get info about each key (from keys-info.json)

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('start');
+const getKeysInfo = async function() {
+    const result = await fetch('./keys-info.json');
+
+    if (!result.ok) {
+        throw 'ERROR' + result.status;
+    }
+
+    return result.json();
+}
+
+getKeysInfo().then(data => console.log(data))
